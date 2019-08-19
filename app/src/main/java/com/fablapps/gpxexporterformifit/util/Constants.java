923af87d68fa4005765787836611fbcab1848999
Ataberk Dönmez <vbbyatta@gmail.com>
2019-08-19 17:42:00 +0300

    Update Constants.java

--- app/src/main/java/com/fablapps/gpxexporterformifit/util/Constants.java
+++ app/src/main/java/com/fablapps/gpxexporterformifit/util/Constants.java
@@ -20,9 +20,9 @@ public class Constants {
     public static final Integer TYPE_CYCLING = 9;
     public static final Integer TYPE_OTHER = 8;
 
-    public static final String LICENSE_KEY_BILLING = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1UjrgkFQLRi2rq+h6WADa46gEVVCJ/fa81ie5n8QrHowDROr+HNrXW6qBXA7zRpKYTtoSqFaxblKDzNlkj2Vn13GzvknDAK+FTazB3vu9qdgK83HJsne24vBAZ9WPKtgG3rdQAChcsxUz3kyxpr7l++Hc0qXecmdtDyJmOw0FwMxJsXx/U0tSPx2ljE5TTjFpiq5Z7k+jQIouZ68mQaG7wpYCNPfuar9afO1LWlcJSL8Q8X0cHYXBLmFYsxN7V7y0R4DW//emfdRuxQU3fp3JlavTPwsYdVL1ZZFI4ZTITmHR84YwSkkQ2P2N/LvgkBwpCiYVtehbqdaatXqg/I4nQIDAQAB";
+    public static final String LICENSE_KEY_BILLING = "MIIBIjAyxpr7l++Hc0qXecmdtDyJmOw0FwMxJsXx/U0tSPx2ljE5TTjFpiq5Z7k+jQIouZ68mQaG7wpYCNPfuar9afO1LWlcJSL8Q8X0cHYXBLmFYsxN7V7y0R4DW//emfdRuxQU3fp3JlavTPwsYdVL1ZZFI4ZTITmHR84YwSkkQ2P2N/LvgkBwpCiYVtehbqdaatXqg/I4nQIDAQAB";
     public static final String NOADS_BILLING_ID = "item_noads";
 
-    public static final String ADMOB_GENERAL_ID = "ca-app-pub-9651595789067045~2830176341";
-    public static final String ADMOB_AD_ID = "ca-app-pub-9651595789067045/3834042926";
+    public static final String ADMOB_GENERAL_ID = "ca-app-pub-9651595789067045~28300176341";
+    public static final String ADMOB_AD_ID = "ca-app-pub-9651595789067045/38340042926";
 }
